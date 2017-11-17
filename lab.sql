fd0add4eea527f955f44a6edc24e3e3b9b810880
ppieso12 <ppieso07@gmail.com>
2017-11-17 16:12:36 +0100

    Update lab.sql

--- lab.sql
+++ lab.sql
@@ -7,11 +7,13 @@ select * from klienci where miejscowosc = 'Warszawa' or miejscowosc = 'Kraków' 
 
 select * from klienci where miejscowosc in ('Warszawa', 'Kraków') order by miejscowosc desc, nazwa asc;
 
+2.1.5
+select * from klienci where miejscowosc in ('Kraków') order by nazwa;
 
 2.2.1
 select nazwa, masa from czekoladki where masa > 20;
 
-select nazwa, masa, koszt from czekoladki where masa > 20 and koszt > 25;
+select nazwa, masa, koszt from czekoladki where masa > 20 and koszt > 0.25;
 
 select nazwa, masa, koszt*100 as koszt from czekoladki where masa > 20 and koszt > 0.25;
 
@@ -20,7 +22,13 @@ select nazwa, masa, koszt*100 as koszt from czekoladki where masa > 20 and koszt
 select nazwa, czekolada, orzechy, nadzienie from czekoladki where czekolada ='mleczna' and nadzienie='maliny' or czekolada ='mleczna' and nadzienie='truskawki' or orzechy='laskowe' and czekolada!='gorzka';
 
 select 2^20 as wynik;
+
+select sqrt(3) as wynik;
+
+select pi() as wynik;
 select masa, nazwa from czekoladki where masa >= 14 and masa <= 24;
+
+select idczekoladki, masa, nazwa, koszt from czekoladki where masa between 15 and 24 or koszt between 0.15 and 0.24;
 
 2.5
 1
@@ -29,10 +37,18 @@ select idczekoladki as idCzekoladki, nazwa, czekolada, orzechy, nadzienie from c
 select idczekoladki as idCzekoladki, nazwa, czekolada, orzechy, nadzienie from czekoladki where orzechy is null;
 3
 select idczekoladki as idCzekoladki, nazwa, czekolada, orzechy, nadzienie from czekoladki where orzechy is not null or nadzienie is not null;
+4
+select idczekoladki as idCzekoladki, nazwa, czekolada, orzechy, nadzienie from czekoladki 
+where czekolada in ('mleczna','biała') and orzechy is null;
 5
-select idczekoladki as idCzekoladki, nazwa, czekolada, orzechy, nadzienie from czekoladki where orzechy is not null or nadzienie is not null;
+select idczekoladki as idCzekoladki, nazwa, czekolada, orzechy, nadzienie from czekoladki 
+where czekolada not in ('mleczna','biała') and (orzechy is not null or nadzienie is not null);
+9
+select idczekoladki as idCzekoladki, nazwa, czekolada, orzechy, nadzienie from czekoladki 
+where czekolada  in ('mleczna','biała') and nadzienie is null;
 
 \o wynik.html
+pierdoły ze skryptami
 3.1
 1
 select idzamowienia, datarealizacji from zamowienia where datarealizacji >= '2013-11-12' and datarealizacji <= '2013-11-20';
@@ -45,22 +61,60 @@ where datarealizacji >= '2013-12-01' and datarealizacji <= '2013-12-31';
 4
 select idzamowienia, datarealizacji from zamowienia
 where extract(year from datarealizacji) = 2013 and extract(month from datarealizacji) = 11;
+5
+select idzamowienia, datarealizacji from zamowienia
+where extract(year from datarealizacji) = 2013 and extract(month from datarealizacji) in (11,12);
+
+6  17, 18 lub 19 dnia miesiąca,
+select idzamowienia, datarealizacji from zamowienia
+where extract(day from datarealizacji) in (17,18,19);
+
+7 - 46 lub 47 tygodniu roku
+select idzamowienia, datarealizacji, extract(week from datarealizacji) as "nr tygodnia"  from zamowienia
+where extract(week from datarealizacji) in (46,47);
+
 3.2
 1
 select idczekoladki, nazwa, czekolada, orzechy, nadzienie from czekoladki
 where nazwa like 'S%';
+select idczekoladki, nazwa, czekolada, orzechy, nadzienie from czekoladki
+where nazwa similar to 'S%';
 2
 select idczekoladki, nazwa, czekolada, orzechy, nadzienie from czekoladki
 where nazwa like 'S%i';
 3
 select idczekoladki, nazwa, czekolada, orzechy, nadzienie from czekoladki
-where nazwa like 'S%_ma%';
+where nazwa like 'S%_m%';
 4
 select idczekoladki, nazwa, czekolada, orzechy, nadzienie from czekoladki
 where nazwa similar to '(A|B|C)%';
 5
 select idczekoladki, nazwa, czekolada, orzechy, nadzienie from czekoladki
 where nazwa similar to '%(O|o|)rzech%';
+6
+select idczekoladki, nazwa, czekolada, orzechy, nadzienie from czekoladki
+where nazwa similar to 'S%m%';
+
+insert into czekoladki(idczekoladki,nazwa,czekolada,orzechy,masa,opis,koszt)
+values('h50','DMoja czeko','biala','laskowe',21,'testowy opis',0.50);
+
+update czekoladki set nazwa = 'Kmoja czeko' 
+where idczekoladki in ('h51');
+
+8 nie rozpoczyna się żadną z liter: 'D'-'K', 'S' i 'T',
+select idczekoladki, nazwa, czekolada, orzechy, nadzienie from czekoladki
+where nazwa similar to '(D|E|F|G|H|I|J|K|S|T)%';
+
+select idczekoladki, nazwa, czekolada, orzechy, nadzienie from czekoladki
+where nazwa not between 'D' and 'K' and nazwa not similar to '(S|T|K)%';
+
+select idczekoladki, nazwa, czekolada, orzechy, nadzienie from czekoladki
+where nazwa > 'D';   - nazwa = Dmoja wchodzi!
+
+10 składa się dokładnie z jednego słowa.
+
+select idczekoladki, nazwa, czekolada, orzechy, nadzienie from czekoladki
+where nazwa not similar to '% %';
 3.3
 1
 select nazwa, miejscowosc from klienci
@@ -74,13 +128,21 @@ select idczekoladki, nazwa, masa, koszt from czekoladki
 where masa >= 15 and masa <= 24
 union
 select idczekoladki, nazwa, masa, koszt from czekoladki
-where koszt >= 0.15 and masa <= 0.24;
+where koszt >= 0.15 and koszt <= 0.24;
+
+select idczekoladki, nazwa, masa, koszt from czekoladki
+where masa >= 15 and masa <= 24 or koszt >= 0.15 and koszt <= 0.24;
+
 2 ale nie
 select idczekoladki, nazwa, masa, koszt from czekoladki
 where masa >= 25 and masa <= 35
 except
 select idczekoladki, nazwa, masa, koszt from czekoladki
 where koszt >= 0.25 and koszt <= 0.35;
+
+select idczekoladki, nazwa, masa, koszt from czekoladki
+where masa >= 25 and masa <= 35 and (koszt < 0.25 or koszt > 0.35);
+
 3
 select idczekoladki, nazwa, masa, koszt from czekoladki
 where masa >= 15 and masa <= 24
@@ -94,17 +156,77 @@ intersect
 select idczekoladki, nazwa, masa, koszt from czekoladki
 where koszt >= 0.25 and koszt <= 0.35;
 
+5
+select idczekoladki, nazwa, masa, koszt from czekoladki
+where masa >= 25 and masa <= 35
+except
+select idczekoladki, nazwa, masa, koszt from czekoladki
+where koszt >= 0.15 and koszt <= 0.24
+except
+select idczekoladki, nazwa, masa, koszt from czekoladki
+where koszt >= 0.29 and koszt <= 0.35;
+
 3.5
-1
+1 identyfikatory klientów, którzy nigdy nie złożyli żadnego zamówienia
+select idklienta from klienci 
+except
+select idklienta from zamowienia;
+
+2 identyfikatory pudełek, które nigdy nie zostały zamówione
+select idpudelka from pudelka 
+except
+select idpudelka from artykuly where idzamowienia is not null;
+
+3 nazwy klientów, czekoladek i pudełek, które zawierają rz (lub Rz),
+select nazwa from klienci where nazwa similar to '%(Rz|rz)%'
+union
+select nazwa from czekoladki where nazwa similar to '%(Rz|rz)%'
+union
+select nazwa from pudelka where nazwa similar to '%(Rz|rz)%';
+
+4 identyfikatory czekoladek, które nie występują w żadnym pudełku.
+select idczekoladki from czekoladki
+except
+select idczekoladki from zawartosc where idpudelka is not null;
+ 
+3.6.1
+
+select idmeczu, gospodarze[1] + gospodarze[2] + gospodarze[3] + coalesce(gospodarze[4], 0) + coalesce(gospodarze[5], 0) 
+as "suma punktów zdobytych przez gospodarzy",
+goscie[1] + goscie[2] + goscie[3] + coalesce(goscie[4], 0) + coalesce(goscie[5], 0)
+as "suma punktów zdobytych przez gosci"
+from siatkowka.statystyki;
+
+3.6.2
+select idmeczu, gospodarze[1] + gospodarze[2] + gospodarze[3] + coalesce(gospodarze[4], 0) + coalesce(gospodarze[5], 0) 
+as "suma punktów zdobytych przez gospodarzy",
+goscie[1] + goscie[2] + goscie[3] + coalesce(goscie[4], 0) + coalesce(goscie[5], 0)
+as "suma punktów zdobytych przez gosci", gospodarze[5], goscie[5]
+from siatkowka.statystyki
+where gospodarze[5] is not null and goscie[5] is not null and gospodarze[5] >15 or goscie[5] >15;
+
+||   KONKATENACJA
+
+3.6.3  identyfikator i wynik meczu w formacie x:y, np. 3:1 (wynik jest pojedynczą kolumną – napisem),
+select idmeczu, gospodarze[1] + gospodarze[2] + gospodarze[3] + coalesce(gospodarze[4], 0) + coalesce(gospodarze[5], 0)
+|| ':' || goscie[1] + goscie[2] + goscie[3] + coalesce(goscie[4], 0) + coalesce(goscie[5], 0) as wynik
+from siatkowka.statystyki; 
+
+3.6.5
+select idmeczu,gospodarze[1], gospodarze[1] + gospodarze[2] + gospodarze[3] + coalesce(gospodarze[4], 0) + coalesce(gospodarze[5], 0)
+from siatkowka.statystyki
+where sqrt(gospodarze[1]) < log(2,gospodarze[1] + gospodarze[2] + gospodarze[3] + coalesce(gospodarze[4], 0) + coalesce(gospodarze[5], 0));
 
 4.1
-SELECT k.nazwa, z.idzamowienia, FROM klienci k, zamowienia z  
-WHERE z.idklienta = k.idklienta;
+SELECT k.nazwa, z.idzamowienia, FROM klienci k, zamowienia z   ILOCZYN KART k X z
 
 4.2
 4.2.1
 SELECT k.nazwa, k.ulica, z.idzamowienia FROM klienci k JOIN zamowienia z
-ON z.idklienta=k.idklienta and k.nazwa similar to '%Antoni';
+ON z.idklienta=k.idklienta where k.nazwa similar to '%Antoni';
+
+SELECT k.nazwa, k.ulica, z.idzamowienia FROM klienci k JOIN zamowienia z
+USING(idklienta) where k.nazwa similar to '%Antoni';
 
 4.2.2
 SELECT k.nazwa, k.ulica, z.idzamowienia FROM klienci k JOIN zamowienia z
@@ -129,9 +251,35 @@ where b.nazwa similar to 'Kremowa fantazja' or b.nazwa similar to 'Kolekcja jesi
 4.3.3
 SELECT k.nazwa, k.ulica, z.idzamowienia, z.datarealizacji FROM klienci k NATURAL JOIN zamowienia z;
 
-4.3.4
+4.3.4  
+złączenie zewnętrzne
+SELECT k.nazwa, k.ulica FROM klienci k LEFT JOIN zamowienia z USING(idklienta)
+where idzamowienia is null;
+
 SELECT k.nazwa, k.ulica FROM klienci k 
 EXCEPT SELECT k.nazwa, k.ulica FROM klienci k NATURAL JOIN zamowienia;
+
+SELECT k.nazwa, k.ulica FROM klienci k 
+EXCEPT SELECT k.nazwa, k.ulica FROM klienci k JOIN zamowienia z using(idklienta);
+
+SELECT k.nazwa, k.ulica FROM klienci k 
+EXCEPT SELECT k.nazwa, k.ulica FROM klienci k JOIN zamowienia z ON k.idklienta = z.idklienta;
+
+4.3.5 złożyli zamówienia z datą realizacji w listopadzie 2013,
+SELECT k.nazwa, k.ulica, z.idzamowienia, z.datarealizacji FROM klienci k NATURAL JOIN zamowienia z
+where extract(year from z.datarealizacji) = 2013 and extract(month from z.datarealizacji) = 11;
+
+4.3.6 zamówili co najmniej 2 sztuki pudełek Kremowa fantazja lub Kolekcja jesienna w ramach jednego zamówienia
+
+SELECT k.nazwa, k.ulica, z.idzamowienia, z.datarealizacji, a.sztuk, p.nazwa FROM klienci k JOIN zamowienia z USING(idklienta) 
+JOIN artykuly a USING(idzamowienia) JOIN pudelka p USING(idpudelka)
+where a.sztuk >= 2 and p.nazwa in ('Kremowa fantazja','Kolekcja jesienna');
+
+4.3.7 zamówili pudełka, które zawierają czekoladki z migdałami.
+SELECT k.nazwa, k.ulica, z.idzamowienia, z.datarealizacji, a.sztuk, p.nazwa, c.orzechy FROM klienci k JOIN zamowienia z USING(idklienta) 
+JOIN artykuly a USING(idzamowienia) JOIN pudelka p USING(idpudelka) JOIN zawartosc zaw USING(idpudelka) JOIN
+czekoladki c USING(idczekoladki)
+where c.orzechy = 'migdały';
 
 4.4.1
 SELECT p.nazwa, p.opis, c.nazwa FROM pudelka p JOIN zawartosc z using(idpudelka) JOIN czekoladki c using(idczekoladki);
@@ -151,8 +299,37 @@ where c.nazwa similar to 'S%';
 
 4.5.3
 
-SELECT p.nazwa, p.opis, c.nazwa as "nazwa cz3koladki" , p.cena FROM pudelka p JOIN zawartosc z using(idpudelka) JOIN czekoladki c using(idczekoladki)
+SELECT p.nazwa, p.opis, c.nazwa as "nazwa cz3koladki" , p.cena FROM pudelka p 
+JOIN zawartosc z using(idpudelka) JOIN czekoladki c using(idczekoladki)
 where count(c.idczekoladki) = 4;
+
+4.5.4 zawierają czekoladki z nadzieniem truskawkowym,
+SELECT p.nazwa, p.opis, c.nazwa as "nazwa cz3koladki" ,c.nadzienie, p.cena FROM pudelka p 
+JOIN zawartosc z using(idpudelka) JOIN czekoladki c using(idczekoladki)
+where c.nadzienie = 'truskawki';
+
+4.5.5 nie zawierają czekoladek w gorzkiej czekoladzie,
+SELECT p.nazwa, p.opis, c.nazwa as "nazwa cz3koladki" ,c.nadzienie,c.czekolada, p.cena FROM pudelka p 
+JOIN zawartosc z using(idpudelka) JOIN czekoladki c using(idczekoladki)
+EXCEPT 
+SELECT p.nazwa, p.opis, c.nazwa as "nazwa cz3koladki" ,c.nadzienie,c.czekolada, p.cena FROM pudelka p 
+JOIN zawartosc z using(idpudelka) JOIN czekoladki c using(idczekoladki)
+where c.czekolada = 'gorzka';
+
+SELECT p.nazwa, p.opis, c.nazwa as "nazwa cz3koladki" ,c.nadzienie,c.czekolada, p.cena FROM pudelka p 
+JOIN zawartosc z using(idpudelka) JOIN czekoladki c using(idczekoladki)
+where c.czekolada != 'gorzka';
+
+4.5.6 zawierają co najmniej 3 sztuki czekoladki Gorzka truskawkowa
+SELECT p.nazwa,c.nadzienie,c.czekolada,c.nazwa, p.cena, z.sztuk FROM pudelka p 
+JOIN zawartosc z using(idpudelka) JOIN czekoladki c using(idczekoladki)
+where c.nazwa = 'Gorzka truskawkowa' and z.sztuk >= 3;
+
+4.6.1 Wyświetl wartości kluczy głównych oraz nazwy czekoladek, których koszt 
+jest większy od kosztu czekoladki o wartości klucza głównego równej d08.
+with p as (select koszt from czekoladki where idczekoladki = 'd08')
+select idczekoladki, nazwa, koszt from czekoladki natural join p;
+
 
 5.1.1
 
